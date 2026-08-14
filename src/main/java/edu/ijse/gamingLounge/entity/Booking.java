a55ec0a2b93fe4bd326e1986e5751094a7adf9a4
Thushal Eranda <madush9989@gmail.com>
2026-08-14 11:42:08 +0530

    added booking entity class

---
+++ src/main/java/edu/ijse/gamingLounge/entity/Booking.java
@@ -0,0 +1,35 @@
+package edu.ijse.gamingLounge.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.gaming_lounge_system.status.BookingStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+    private Double totalAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "station_id")
+    private Station station;
+}
