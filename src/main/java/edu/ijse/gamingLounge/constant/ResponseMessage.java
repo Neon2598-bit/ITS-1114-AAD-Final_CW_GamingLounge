2e969bc33b3b6963de3d54abb710ab723be89888
Thushal Eranda <madush9989@gmail.com>
2026-08-14 11:36:16 +0530

    added response msg class

---
+++ src/main/java/edu/ijse/gamingLounge/constant/ResponseMessage.java
@@ -0,0 +1,7 @@
+package edu.ijse.gamingLounge.constant;
+
+public class ResponseMessage {
+    public static String SUCCESS = "Success";
+    public static String FAILED = "Failed";
+    public static String NOT_FOUND = "Record not found";
+}
