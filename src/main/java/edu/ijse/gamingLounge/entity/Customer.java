ddac9048557969867be3fc00a052186548d79d11
Thushal Eranda <madush9989@gmail.com>
2026-08-14 11:51:22 +0530

    added customer entity class

---
+++ src/main/java/edu/ijse/gamingLounge/entity/Customer.java
@@ -0,0 +1,29 @@
+package edu.ijse.gamingLounge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String phone;
+    
+    private String password;
+
+    private String address;
+
+    @Column(nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
+    private boolean emailVerified = false;
+}
