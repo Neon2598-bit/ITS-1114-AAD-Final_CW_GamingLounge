e0feec46a2de2fb33eef25244122dc06ba8444c6
Thushal Eranda <madush9989@gmail.com>
2026-08-14 11:21:11 +0530

    Project created

---
+++ src/main/java/edu/ijse/gamingLounge/GamingLoungeApplication.java
@@ -0,0 +1,13 @@
+package edu.ijse.gamingLounge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingLoungeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingLoungeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ijse/gamingLounge/GamingLoungeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ijse.gamingLounge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingLoungeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
