d11ae5b354ffb2d3e9cea0898a2579614af47dd0
Thushal Eranda <madush9989@gmail.com>
2026-08-15 12:03:06 +0530

    snack category controller post mapping done

---
+++ src/main/java/edu/ijse/gamingLounge/controller/SnackCategoryController.java
@@ -0,0 +1,27 @@
+package edu.ijse.gamingLounge.controller;
+
+import edu.ijse.gamingLounge.constant.CommonResponse;
+import edu.ijse.gamingLounge.constant.ResponseCode;
+import edu.ijse.gamingLounge.constant.ResponseMessage;
+import edu.ijse.gamingLounge.dto.SnackCategoryDTO;
+import edu.ijse.gamingLounge.service.SnackCategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/snack-category")
+@RequiredArgsConstructor
+@CrossOrigin
+public class SnackCategoryController {
+    private final SnackCategoryService snackCategoryService;
+
+    @PostMapping
+    public ResponseEntity<CommonResponse> save(@Valid @RequestBody SnackCategoryDTO dto) {
+        snackCategoryService.saveCategory(dto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CommonResponse(ResponseCode.CREATED, ResponseMessage.SUCCESS));
+    }
+}
