e3903fcadf546eafd12f7c7e6e8d2d89f0a36922
Thushal Eranda <madush9989@gmail.com>
2026-08-15 21:28:35 +0530

    added put mapping to station controller

--- src/main/java/edu/ijse/gamingLounge/controller/StationController.java
+++ src/main/java/edu/ijse/gamingLounge/controller/StationController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/station")
@@ -25,5 +22,12 @@ public class StationController {
         stationService.saveStation(dto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new CommonResponse(ResponseCode.CREATED, ResponseMessage.SUCCESS));
+    }
+
+    @PutMapping
+    public ResponseEntity<CommonResponse> update(@Valid @RequestBody StationDTO dto) {
+        stationService.updateStation(dto);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new CommonResponse(ResponseCode.SUCCESS, ResponseMessage.SUCCESS));
     }
 }
