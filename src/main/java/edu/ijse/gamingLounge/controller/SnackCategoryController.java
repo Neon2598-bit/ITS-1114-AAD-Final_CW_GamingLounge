a5f4eccd5795468640a895d5a9473e3effebe8eb
Thushal Eranda <madush9989@gmail.com>
2026-08-15 12:07:40 +0530

    added delete mapping snack category controller

--- src/main/java/edu/ijse/gamingLounge/controller/SnackCategoryController.java
+++ src/main/java/edu/ijse/gamingLounge/controller/SnackCategoryController.java
@@ -31,4 +31,11 @@ public class SnackCategoryController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new CommonResponse(ResponseCode.SUCCESS, ResponseMessage.SUCCESS));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CommonResponse> delete(@PathVariable Long id) {
+        snackCategoryService.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new CommonResponse(ResponseCode.SUCCESS, ResponseMessage.SUCCESS));
+    }
 }
