ecd46af58fedc1af6bfc31bf50b5704366aafa3f
Thushal Eranda <madush9989@gmail.com>
2026-08-15 21:25:45 +0530

    created station controller and added post mapping save

---
+++ src/main/java/edu/ijse/gamingLounge/controller/StationController.java
@@ -0,0 +1,29 @@
+package edu.ijse.gamingLounge.controller;
+
+import edu.ijse.gamingLounge.constant.CommonResponse;
+import edu.ijse.gamingLounge.constant.ResponseCode;
+import edu.ijse.gamingLounge.constant.ResponseMessage;
+import edu.ijse.gamingLounge.dto.StationDTO;
+import edu.ijse.gamingLounge.service.StationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/station")
+@RequiredArgsConstructor
+@CrossOrigin
+public class StationController {
+    private final StationService stationService;
+
+    public ResponseEntity<CommonResponse> save(@Valid @RequestBody StationDTO dto){
+        stationService.saveStation(dto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CommonResponse(ResponseCode.CREATED, ResponseMessage.SUCCESS));
+    }
+}
