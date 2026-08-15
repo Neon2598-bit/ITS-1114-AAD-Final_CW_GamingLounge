bdcfcc3197cd382387a5f5262f4bb116638f6fd7
Thushal Eranda <madush9989@gmail.com>
2026-08-15 12:05:33 +0530

    added put mapping for snack category

--- src/main/java/edu/ijse/gamingLounge/controller/SnackCategoryController.java
+++ src/main/java/edu/ijse/gamingLounge/controller/SnackCategoryController.java
@@ -24,4 +24,11 @@ public class SnackCategoryController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new CommonResponse(ResponseCode.CREATED, ResponseMessage.SUCCESS));
     }
+
+    @PutMapping
+    public ResponseEntity<CommonResponse> update(@Valid @RequestBody SnackCategoryDTO dto) {
+        snackCategoryService.updateCategory(dto);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new CommonResponse(ResponseCode.SUCCESS, ResponseMessage.SUCCESS));
+    }
 }
