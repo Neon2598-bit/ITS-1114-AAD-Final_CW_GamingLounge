de4fd1087bb71c4f9188a17093d8a2b7242b29a2
Thushal Eranda <madush9989@gmail.com>
2026-08-15 12:11:20 +0530

    added get mapping for snack category controller

--- src/main/java/edu/ijse/gamingLounge/controller/SnackCategoryController.java
+++ src/main/java/edu/ijse/gamingLounge/controller/SnackCategoryController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/snack-category")
 @RequiredArgsConstructor
@@ -37,5 +39,16 @@ public class SnackCategoryController {
         snackCategoryService.deleteCategory(id);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new CommonResponse(ResponseCode.SUCCESS, ResponseMessage.SUCCESS));
+    }
+
+    @GetMapping
+    public ResponseEntity<CommonResponse> getAll() {
+        List<SnackCategoryDTO> list = snackCategoryService.getAllCategories();
+        if (list.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new CommonResponse(ResponseCode.NOT_FOUND, ResponseMessage.NOT_FOUND));
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new CommonResponse(ResponseCode.SUCCESS, list, ResponseMessage.SUCCESS));
     }
 }
