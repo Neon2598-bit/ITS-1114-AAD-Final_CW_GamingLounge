e042fbfa4bb28de7f2c67291034f230580c1cacb
Thushal Eranda <madush9989@gmail.com>
2026-08-15 13:58:38 +0530

    creted and implemented snack controller with post mapping

---
+++ src/main/java/edu/ijse/gamingLounge/controller/SnackController.java
@@ -0,0 +1,27 @@
+package edu.ijse.gamingLounge.controller;
+
+import edu.ijse.gamingLounge.constant.CommonResponse;
+import edu.ijse.gamingLounge.constant.ResponseCode;
+import edu.ijse.gamingLounge.constant.ResponseMessage;
+import edu.ijse.gamingLounge.dto.SnackDTO;
+import edu.ijse.gamingLounge.service.SnackService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/snack")
+@RequiredArgsConstructor
+@CrossOrigin
+public class SnackController {
+    private final SnackService snackService;
+
+    @PostMapping
+    public ResponseEntity<CommonResponse> save(@Valid @RequestBody SnackDTO dto) {
+        snackService.saveSnack(dto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CommonResponse(ResponseCode.CREATED, ResponseMessage.SUCCESS));
+    }
+}
