b3a5ed70a5acab5dc5f24892e6479585203a4bd0
Thushal Eranda <madush9989@gmail.com>
2026-08-16 12:26:58 +0530

    finished membership saving method

--- src/main/java/edu/ijse/gamingLounge/repository/MembershipRepository.java
+++ src/main/java/edu/ijse/gamingLounge/repository/MembershipRepository.java
@@ -1,9 +1,11 @@
 package edu.ijse.gamingLounge.repository;
 
 import edu.ijse.gamingLounge.entity.Membership;
+import edu.ijse.gamingLounge.status.MembershipStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    boolean existsByCustomer_IdAndStatus(Long customerId, MembershipStatus status);
 }
--- src/main/java/edu/ijse/gamingLounge/service/MembershipServiceImpl.java
+++ src/main/java/edu/ijse/gamingLounge/service/MembershipServiceImpl.java
@@ -1,18 +1,59 @@
 package edu.ijse.gamingLounge.service;
 
 import edu.ijse.gamingLounge.dto.MembershipDTO;
+import edu.ijse.gamingLounge.entity.Customer;
+import edu.ijse.gamingLounge.entity.Membership;
+import edu.ijse.gamingLounge.entity.MembershipPlan;
+import edu.ijse.gamingLounge.exception.BusinessException;
+import edu.ijse.gamingLounge.repository.CustomerRepository;
+import edu.ijse.gamingLounge.repository.MembershipPlanRepository;
+import edu.ijse.gamingLounge.repository.MembershipRepository;
+import edu.ijse.gamingLounge.status.MembershipStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class MembershipServiceImpl implements MembershipService {
+    private final MembershipRepository membershipRepository;
+    private final CustomerRepository customerRepository;
+    private final MembershipPlanRepository membershipPlanRepository;
+
     @Override
     public void saveMembership(MembershipDTO dto) {
+        Optional<Customer> customerOptional = customerRepository.findById(dto.getCustomerId());
+        Optional<MembershipPlan> membershipPlanOptional = membershipPlanRepository.findById(dto.getMembershipPlanId());
+
+        if (customerOptional.isEmpty() || membershipPlanOptional.isEmpty()) {
+            log.error("Customer Or Membership Plan Does Not Exist");
+            throw new BusinessException("Customer Or Membership Plan Does Not Exist");
+        }
+
+        if (membershipRepository.existsByCustomer_IdAndStatus(dto.getCustomerId(), MembershipStatus.ACTIVE)){
+            throw new BusinessException("You already have an active membership");
+        }
+
+        MembershipPlan membershipPlan = membershipPlanOptional.get();
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = startDate.plusDays(membershipPlan.getDurationDays());
+
+        Membership membership = new Membership();
+
+        membership.setCustomer(customerOptional.get());
+        membership.setMembershipPlan(membershipPlan);
+        membership.setStartDate(startDate);
+        membership.setEndDate(endDate);
+        membership.setStatus(MembershipStatus.ACTIVE);
+
+        membershipRepository.save(membership);
+
+        log.info("Membership Saved Successfully");
 
     }
 
