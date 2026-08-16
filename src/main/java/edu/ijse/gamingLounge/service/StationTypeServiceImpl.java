a671b61a52497736e71441237acea2d7954544ed
Thushal Eranda <madush9989@gmail.com>
2026-08-16 09:25:00 +0530

    override methods implemented but blank

--- src/main/java/edu/ijse/gamingLounge/service/StationTypeServiceImpl.java
+++ src/main/java/edu/ijse/gamingLounge/service/StationTypeServiceImpl.java
@@ -1,4 +1,37 @@
 package edu.ijse.gamingLounge.service;
 
+import edu.ijse.gamingLounge.dto.StationTypeDTO;
+
+import java.util.List;
+
 public class StationTypeServiceImpl implements StationTypeService{
+    @Override
+    public void saveStationType(StationTypeDTO stationTypeDTO) {
+        
+    }
+
+    @Override
+    public void updateStationType(StationTypeDTO stationTypeDTO) {
+
+    }
+
+    @Override
+    public void deleteStationType(Long id) {
+
+    }
+
+    @Override
+    public List<StationTypeDTO> getAllStationTypes() {
+        return List.of();
+    }
+
+    @Override
+    public StationTypeDTO getStationTypeById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<StationTypeDTO> getAffordableTypes(Double maxRate) {
+        return List.of();
+    }
 }
