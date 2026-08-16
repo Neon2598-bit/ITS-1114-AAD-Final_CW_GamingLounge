da961b0fad76f67ac194001418b10dad6936bccc
Thushal Eranda <madush9989@gmail.com>
2026-08-16 09:29:27 +0530

    added annotations for station type service impl and save method

--- src/main/java/edu/ijse/gamingLounge/service/StationTypeServiceImpl.java
+++ src/main/java/edu/ijse/gamingLounge/service/StationTypeServiceImpl.java
@@ -1,13 +1,32 @@
 package edu.ijse.gamingLounge.service;
 
 import edu.ijse.gamingLounge.dto.StationTypeDTO;
+import edu.ijse.gamingLounge.entity.StationType;
+import edu.ijse.gamingLounge.repository.StationTypeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class StationTypeServiceImpl implements StationTypeService{
+    private final StationTypeRepository stationTypeRepository;
+
     @Override
     public void saveStationType(StationTypeDTO stationTypeDTO) {
-        
+        try {
+            StationType stationType = new StationType();
+            stationType.setTypeName(stationTypeDTO.getTypeName());
+            stationType.setHourlyRate(stationTypeDTO.getHourlyRate());
+            stationTypeRepository.save(stationType);
+            log.info("StationType save successful");
+
+        } catch (Exception ex) {
+            log.error("StationType save failed");
+        }
     }
 
     @Override
